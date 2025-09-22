078d9ed3502e1c6296a67836dbb81b05072d3960
FrostHamilton <FrostHamilton@users.noreply.github.com>
2025-09-22 13:26:56 +0000

    deploy: 6044ad49e7b356fdebeb8b8567036149d7d21fb1

--- assets/zh-cn_briefing.md.7_8ae7uz.lean.js
+++ assets/zh-cn_briefing.md.7_8ae7uz.lean.js
@@ -1,4 +1,4 @@
-import{v as l,c as i,o,ah as d}from"./chunks/framework.CyOEM546.js";const b=JSON.parse('{"title":"前言","description":"","frontmatter":{},"headers":[],"relativePath":"zh-cn/briefing.md","filePath":"zh-cn/briefing.md","lastUpdated":1758535746000}'),h={name:"zh-cn/briefing.md"},g=Object.assign(h,{setup(s){return l(async()=>{try{const n=(await(await fetch("https://v2.xxapi.cn/api/weather?city=香洲")).json()).data.data;let a=`<table border="1" cellspacing="0" cellpadding="5">
+import{v as l,c as i,o,ah as d}from"./chunks/framework.CyOEM546.js";const b=JSON.parse('{"title":"💾 前言","description":"","frontmatter":{},"headers":[],"relativePath":"zh-cn/briefing.md","filePath":"zh-cn/briefing.md","lastUpdated":1758547469000}'),h={name:"zh-cn/briefing.md"},g=Object.assign(h,{setup(s){return l(async()=>{try{const n=(await(await fetch("https://v2.xxapi.cn/api/weather?city=香洲")).json()).data.data;let a=`<table border="1" cellspacing="0" cellpadding="5">
                   <tr>
                     <th>日期</th><th>天气</th><th>温度</th><th>风向</th><th>空气质量</th>
                   </tr>`;n.forEach(t=>{a+=`<tr>
--- assets/zh-cn_news_Ragasa.md.BZ0vydyz.lean.js
+++
@@ -1,7 +0,0 @@
-import{v as r,c as n,o as s,ah as i}from"./chunks/framework.CyOEM546.js";const c=JSON.parse('{"title":"🌀 超强台风“桦加沙”实时资讯","description":"","frontmatter":{},"headers":[],"relativePath":"zh-cn/news/Ragasa.md","filePath":"zh-cn/news/Ragasa.md","lastUpdated":1758546890000}'),p={name:"zh-cn/news/Ragasa.md"},d=Object.assign(p,{setup(l){return r(async()=>{try{const a=(await(await fetch("https://free.xwteam.cn/api/weather/typhoon")).json()).data;let o=`
-      <p><b>中文名：</b>${a.cn_name}</p>
-      <p><b>英文名：</b>${a.en_name}</p>
-      <p><b>编号：</b>${a.windid}</p>
-      <p><b>更新时间：</b>${a.end_time}</p>
-      <p><b>位置：</b>${a.location}</p>
-    `;document.getElementById("typhoon").innerHTML=o}catch(e){document.getElementById("typhoon").innerHTML="获取数据失败",console.error(e)}}),(e,t)=>(s(),n("div",null,t[0]||(t[0]=[i("",27)])))}});export{c as __pageData,d as default};
